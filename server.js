101550d378f54d26958a9a50da56d372ef7aa054
tunglam241 <20521521@gm.uit.edu.vn>
2023-12-08 09:54:51 +0700

    add file readme

--- server.js
+++ server.js
@@ -66,11 +66,9 @@ app.post("/api/v1/products", async (req, res) => {
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
-    console.log(error.message);
     res.status(500).json({ message: error.message });
   }
-  console.log(req.body);
-  res.send(req.body);
+  //res.send(req.body);
 });
 
 mongoose.set("strictQuery", false);
